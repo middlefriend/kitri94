51901d5fde07cb53a47416f4542961af89966e87
RyuJihyun0723 <76501174+RyuJihyun0723@users.noreply.github.com>
2021-07-09 11:34:36 +0900

    찐막...오타수정..

--- project_test/src/server/program/Server.java
+++ project_test/src/server/program/Server.java
@@ -4,29 +4,28 @@ import java.io.*;
 import java.net.*;
 import java.util.HashSet;
 
+import client.frame.LoginFrame;
 import server.frame.ServerFrame;
 
 
 public class Server {
 	
 	public static HashSet<ServerHandler> serverSet = new HashSet<ServerHandler>();
-	public static ServerFrame frame;
-	
+	public static LoginFrame lframe;
 	static boolean[] seatStat = new boolean[20];
 	
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
 		//서버 port
 		final int serverPort = 7777;
-		
-		frame = new ServerFrame();
+		lframe = new LoginFrame();
 		try {
 			serverSocket = new ServerSocket(serverPort);
 			System.out.println("서버 시작");
 			while(true) {
 				//서버 소켓 연결
 				Socket socket = serverSocket.accept();
-				ServerHandler serverHandler= new ServerHandler(socket);
+				ServerHandler serverHandler= new ServerHandler(socket, lframe);
 				serverSet.add(serverHandler);
 				System.out.println("클라이언트 접속 완료 -("+serverSet.size()+")");
 				
