1cc608dc5bb0b02f298ba2389d66d961f1eac1e6
ohkyeongha <okh970526@gmail.com>
2021-07-09 14:15:10 +0900

    check

--- project_test/src/server/program/Server.java
+++ project_test/src/server/program/Server.java
@@ -1,42 +1,42 @@
 package server.program;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.HashSet;
 
+import client.frame.LoginFrame;
 import server.frame.ServerFrame;
 
+
 public class Server {
-
+	
 	public static HashSet<ServerHandler> serverSet = new HashSet<ServerHandler>();
-	public static ServerFrame frame;
-
+	public static LoginFrame lframe;
 	static boolean[] seatStat = new boolean[20];
-
+	
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
-		// 서버 port
+		//서버 port
 		final int serverPort = 7777;
-
-		frame = new ServerFrame();
+		lframe = new LoginFrame();
 		try {
 			serverSocket = new ServerSocket(serverPort);
 			System.out.println("서버 시작");
-			while (true) {
-				// 서버 소켓 연결
+			while(true) {
+				//서버 소켓 연결
 				Socket socket = serverSocket.accept();
-				ServerHandler serverHandler = new ServerHandler(socket);
+				ServerHandler serverHandler= new ServerHandler(socket, lframe);
 				serverSet.add(serverHandler);
-				System.out.println("클라이언트 접속 완료 -(" + serverSet.size() + ")");
-
-				// 출력 스레드 : 로그인, 회원가입, 좌석선택
+				System.out.println("클라이언트 접속 완료 -("+serverSet.size()+")");
+				
+				
+				//출력 스레드 : 로그인, 회원가입, 좌석선택
 				Thread serverHandlerT = new Thread(serverHandler);
 				serverHandlerT.start();
 //				Thread serverTimer = new Thread(new ServerTimer());
 //				serverTimer.start();
 			}
-		} catch (IOException e) {
+		}catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
--- project_test/src/server/program/ServerHandler.java
+++ project_test/src/server/program/ServerHandler.java
@@ -1,90 +1,120 @@
 package server.program;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
 
+import client.frame.LoginFrame;
 import message.Message;
-import pcuser.UserDAO;
+import server.frame.*;
+import pcuser.*;
 
-public class ServerHandler implements Runnable {
-	// 입력 스트림
-	public ObjectInputStream ois;
-	// 출력 스트림
-	public ObjectOutputStream oos;
+public class ServerHandler implements Runnable{
+	public ObjectInputStream ois;	//입력 스트림
+	public ObjectOutputStream oos;	//출력 스트림
 
+	public LoginFrame frame;
+	public UserDAO dao = new UserDAO();
+	
 	Socket socket;
-	int seat;
-
-	public ServerHandler(Socket socket) {
+	int seatNum = 0;	//배정 받은 좌석 번호(신규일 때 0)
+	
+	public ServerHandler(Socket socket,LoginFrame frame) {
 		this.socket = socket;
+		this.frame = frame;
 	}
-
+	
 	public void run() {
-		System.out.println("서버 핸들러 시작-" + socket);
+		System.out.println("서버 핸들러 시작-"+socket);
 		try {
-			// 입력 스트림 생성
+			//입출력 스트림 생성
 			oos = new ObjectOutputStream(socket.getOutputStream());
 			ois = new ObjectInputStream(socket.getInputStream());
-			System.out.println(ois + "," + oos + "생성");
-
+			System.out.println(ois+","+oos+"생성");
+			
 			Message inMsg = null;
-			Object obj = null;
-
-			while ((obj = ois.readObject()) != null) {
-				if (obj instanceof Message) {
-					inMsg = (Message) obj;
-					Message outMsg = new Message();
-					UserDAO udao = new UserDAO();
-					// 클라이언트로 부터 입력
-					int stat = inMsg.getState();
-
-					switch (stat) {
-					case 1: { // ID 중복확인
-						String id = inMsg.getUvo().getUserID();
-						int result = udao.checkID(id);
-						// state - idcheck에 대한 결과 값으로 1입력
-						outMsg.setState(1);
-						// result
-						outMsg.setResult(result);
-						// writeObject
-						oos.writeObject(outMsg);
-						oos.flush();
-
-					}
-					case 2: {// 회원가입
-						outMsg = new Message();
-						int result = udao.insertUser(inMsg.getUvo().getUserID(), inMsg.getUvo().getPwd(),
-								inMsg.getUvo().getName());
-						outMsg.setState(2);
-						outMsg.setResult(result);
-						oos.writeObject(outMsg);
-						oos.flush();
-					}
-					case 3: {// 로그인
-						outMsg = new Message();
-						int result = udao.getAuth(inMsg.getUvo().getUserID(), inMsg.getUvo().getPwd());
-						outMsg.setState(3);
-						outMsg.setResult(result);
-						oos.writeObject(outMsg);
-						oos.flush();
-					}
-					case 4: {// 좌석이동
-
-					}
-					case 5: {// 시간충전
-						outMsg = new Message();
-						int result = udao.chargeTime(inMsg.getUvo().getUserID(), inMsg.getTime());
-						outMsg.setState(5);
-						outMsg.setResult(result);
-						oos.writeObject(outMsg);
-						oos.flush();
-					}
+			Object obj=null;
+			
+			while((obj=ois.readObject())!=null) {
+				if(obj instanceof Message) {
+					//클라이언트로 부터 입력받은 메세지
+					inMsg = (Message)obj;
+					System.out.println(inMsg);
+					int state = inMsg.getState();
+					switch(state) {
+						case 1: {	// ID 중복확인
+							int result = dao.checkID(inMsg.getUserID());
+							Message outMsg = new Message();
+							outMsg.setState(1);
+							outMsg.setResult(result);
+							oos.writeObject(outMsg);
+							break;
+						}
+						case 2: {	// 회원가입
+							int result = dao.insertUser(inMsg.getUserID(),inMsg.getPwd(),inMsg.getName());
+							Message outMsg = new Message();
+							outMsg.setState(2);
+							outMsg.setResult(result);
+							oos.writeObject(outMsg);
+							break;
+						}
+						case 3: {	// 로그인
+							int result = dao.getAuth(inMsg.getUserID(),inMsg.getPwd());
+							Message outMsg = new Message();
+							// if(result == 1){
+								//로그인 성공 시 응답
+								//남은 시간 전송
+								outMsg.setState(3);
+								outMsg.setResult(result);
+								oos.writeObject(outMsg);
+							// }else{
+								//로그인 실패 시 응답
+							// }
+							break;
+						}
+//						case 4: {	// 좌석 배정 / 이동
+//							//배정 받으려는 좌석 번호
+//							int newSeatNum = inMsg.getSeatNum();
+//							Message outMsg = new Message();
+//							if(Server.seatMap.containsKey(newSeatNum)){
+//								//해당 좌석 사용 중
+//								//배정 실패 응답
+//								outMsg.setState(4);
+//								outMsg.setResult(0);
+//								oos.writeObject(outMsg);
+//								break;
+//							}
+//							//좌석 배정
+//							Server.seatMap.put(newSeatNum,this);
+//							//기존 좌석이 있을 때 회수
+//							if(Server.seatMap.containsKey(seatNum)){
+//								Server.seatMap.remove(seatNum);	
+//							}
+//							seatNum = newSeatNum;
+//							//배정 성공 응답
+//							outMsg.setResult(1);
+//							oos.writeObject(outMsg);
+//							break;
+//						}
+						case 5: {	// 시간충전
+							int time = inMsg.getRemain();
+							int result = dao.chargeTime(inMsg.getUserID(),time);	//time 만큼 기존 시간에 추가
+							Message outMsg = new Message();
+							// if(result == 1){		//시간 충전 성공
+								//남은 시간 전송
+								outMsg.setState(5);
+								outMsg.setResult(result);
+								outMsg.setRemain(time);
+								oos.writeObject(outMsg);
+							// }else{
+								//시간 충전 실패
+							// }
+							break;
+						}
 					}
 				}
 			}
-
-		} catch (Exception e) {
+			
+		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
